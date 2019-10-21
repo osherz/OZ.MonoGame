3d3c8cd3493fa19708372deb62e6210fc809a7fb
Osher <osherz92@gmail.com>
2019-10-21 23:45:25 +0300

    IControlApearance edited

--- GameObjects/UI/IControlApearance.cs
+++ GameObjects/UI/IControlApearance.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Text;
+
+
 
 namespace OZ.MonoGame.GameObjects.UI
 {
     public interface IControlApearance
     {
-
+        Texture2D Reg { get; }
+        Texture2D MouseHover { get; }
+        Texture2D MouseDown { get; }
+        Texture2D NotEnable { get; }
     }
 }
